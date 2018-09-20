322e70715a9bbc9765543c295a5d6ed45b82b9d8
Nishtha Bhatia <nbhatia329@gmail.com>
2018-09-20 22:28:53 +0530

    Init, dependencies added

---
+++ app/src/main/java/com/apps/nb2998/architectureexample/MainActivity.java
@@ -0,0 +1,12 @@
+package com.apps.nb2998.architectureexample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
